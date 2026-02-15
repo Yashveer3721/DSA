847f3beb6ce4a7345244dd089d98e2135538ae11
Yashveer3721 <yashveer100iit@gmail.com>
2026-02-15 23:26:47 +0530

    Time: 2 ms (17.24%) | Memory: 8.8 MB (83.36%) - LeetSync

---
+++ 4177-reverse-string-prefix/reverse-string-prefix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reversePrefix(string s, int k) {
+        int start=0,end=k-1;
+        while(start<end)
+        {
+            swap(s[start],s[end]);
+            start++,end--;
+        }
+        return s;
+    }
+};
