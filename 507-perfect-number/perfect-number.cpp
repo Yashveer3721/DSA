b4330edeeafe4ca052d0844d0b6d12bde4345152
Yashveer3721 <yashveer100iit@gmail.com>
2026-04-04 22:11:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (20.22%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum=0;
-        for(int i=1;i<num;i++)
-        {
-            if(num%i==0)
-            sum+=i;
+        if(num <= 1) return false;  // no perfect number <= 1
+        
+        int sum = 1; // 1 is always a divisor
+        for(int i = 2; i * i <= num; i++) {
+            if(num % i == 0) {
+                sum += i;
+                if(i != num / i) sum += num / i; // add the paired divisor
+            }
         }
-        if(sum==num)
-        return true;
-        else
-        return false;
+        return sum == num;
     }
 };
