573bd0d2886f30356b3d1c109ae5299c9c871dc0
Yashveer3721 <yashveer100iit@gmail.com>
2026-04-11 18:13:45 +0530

    Time: 2 ms (81.02%) | Memory: 13.1 MB (82.62%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    void backtrack(vector<string>& res, string cur, int open, int close, int n) {
-        if(cur.size() == 2*n) {
-            res.push_back(cur);
-            return;
+    void generate(int open, int close, string &path, vector<string> &ans) {
+        if(open>close || open<0) 
+        return;
+        if(open==0 && close==0)
+        {
+            ans.push_back(path);
+            return ;
         }
-        if(open < n) backtrack(res, cur + "(", open + 1, close, n);
-        if(close < open) backtrack(res, cur + ")", open, close + 1, n);
+        //push open parenthesis
+        path.push_back('(');
+        generate(open-1,close,path,ans);
+        path.pop_back();
+        //push close parenthesis
+        path.push_back(')');
+        generate(open,close-1,path,ans);
+        path.pop_back();
+
     }
     
     vector<string> generateParenthesis(int n) {
-        vector<string> res;
-        backtrack(res, "", 0, 0, n);
-        return res;
+        vector<string> ans;
+        string temp;
+        generate(n,n,temp,ans);
+        return ans;
     }
 };
