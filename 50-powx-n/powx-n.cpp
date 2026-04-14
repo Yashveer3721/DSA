fd9bf043276ed35b498024d57e3efd94b0c20031
Yashveer3721 <yashveer100iit@gmail.com>
2026-04-14 23:04:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.70%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    double myPow(double x, long n) {
+        if (n == 0) return 1.0;
+        if (n < 0) return 1.0 / myPow(x, -n);
+        if (n % 2 == 0) return myPow(x * x, n / 2);
+        else return x * myPow(x, n - 1);
+    }
+};
